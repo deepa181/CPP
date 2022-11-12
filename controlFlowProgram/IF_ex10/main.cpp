85ee83edef70cc26c2b5211bde3c0824d2fcda69
deepa181 <47942722+deepa181@users.noreply.github.com>
2022-11-12 15:32:07 +0530

    Add files via upload

---
+++ controlFlowProgram/IF_ex10/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void can_you_drive(int age) {
+    //----WRITE YOUR CODE BELOW THIS LINE----
+    
+    if(age>=16)
+    cout<<"Yes - you can drive!";
+    
+    //----WRITE YOUR CODE ABOVE THIS LINE----
+}
+int main()
+{
+    can_you_drive(17);
+    return 0;
+}
